cd843a57d3374937407b140dcf736bf2ca7e4da6
Jacek Kawiak <jacek.kawiak@gmail.com>
2023-06-13 14:27:54 +0200

    Added icon to start screen button

---
+++ lib/start_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_right_alt,
+            ),
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
